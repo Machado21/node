152cc24c3bc795dc7a4df789ab1cd55dda7c1f50
brunorcx <brunorcx@hotmail.com>
2021-08-22 14:29:13 -0400

    upload de imagem só com o buffer

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -27,11 +27,14 @@ module.exports = {
   },
   //Create and store an article
   async store(req, res) {
-    console.log(req);
+    let produtoNovo = req.body;
+    produtoNovo["img"] = req.file.buffer;
+    console.log(req.body);
     console.log("Req file ");
     console.log(req.file);
-    let produtoNovo = req.body;
-    produtoNovo["img"] = req.file;
+    // let produtoNovo = req.body;
+    // produtoNovo["img"] = req.file;
+    // console.log(req.file.buffer);
 
     //req.body e produto são objetos javascript, portanto não podem ser iterados como um vetor
     //Assim tive de iterar por um e pegar as propriedades do outro
@@ -40,8 +43,8 @@ module.exports = {
     //(2) passar para um vetor todos os valores
     //(3) iterar no OUTRO objeto e atribuir a partir de um índice do vetor copiado anteriormente
 
-    Products.create(req.body).then((result) => {
-      console.log(req.body);
+    Products.create(produtoNovo).then((result) => {
+      // console.log(req.body);
       console.log(result);
       // return res
       //   .json({
